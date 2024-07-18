94e437e62000bd92ca9c5635c3fb20cc3395b766
Katyani27 <154884424+Katyani27@users.noreply.github.com>
2024-07-18 14:29:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-// your code here
+    if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    }
+
+    let a = 0, b = 1;
+    for (let i = 3; i <= num; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 }
+
+// Example usage:
+console.log(fibonacci(1));  // Output: 0
+console.log(fibonacci(5));  // Output: 3
+
 
 module.exports = fibonacci;
